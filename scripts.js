925a6880ac09dc87a2908d7407eb7cbfbed9a796
UmmeHanee1416 <73064213+UmmeHanee1416@users.noreply.github.com>
2026-01-18 13:03:54 +0600

    added: navbar

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#name').addEventListener("click", function(){
+  fetch('navbar.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('navbar').innerHTML = data);
+    
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#name').addEventListener("click", function () {
         this.style.backgroundColor = "black";
         this.style.color = "white";
     });
-    document.querySelector('#email').addEventListener("click", function(){
+    document.querySelector('#email').addEventListener("click", function () {
         this.style.backgroundColor = "black";
         this.style.color = "white";
     });
-    document.querySelector('#message').addEventListener("click", function(){
+    document.querySelector('#message').addEventListener("click", function () {
         this.style.backgroundColor = "black";
         this.style.color = "white";
     });
-    document.querySelector('form').addEventListener('submit', function(event) {
+    document.querySelector('form').addEventListener('submit', function (event) {
         // alert('hello, ' + document.querySelector('#name').value);
         document.querySelector('#name').value = '';
         document.querySelector('#email').value = '';
@@ -20,3 +25,15 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();
     });
 });
+
+function downloadFile(e) {
+    e.preventDefault();
+    const link = document.createElement('a');
+    link.href = 'UmmeHanee_CV.pdf';
+    link.download = 'ResumeOfUmmeHanee.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    console.log('Download initiated');
+
+}
